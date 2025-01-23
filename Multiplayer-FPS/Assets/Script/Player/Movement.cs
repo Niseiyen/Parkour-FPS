7cd70714be83fb97907193711a861eb9609af6c8
Niseiyen <127323783+Niseiyen@users.noreply.github.com>
2025-01-23 08:10:50 -0500

    Ajout de Sauter et courir

--- Multiplayer-FPS/Assets/Script/Player/Movement.cs
+++ Multiplayer-FPS/Assets/Script/Player/Movement.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    [SerializeField] private float walkSpeed = 4f;
+    [SerializeField] private float walkSpeed = 8f;
+    [SerializeField] private float runSpeed = 14f;
     [SerializeField] private float maxVelocityChange = 10.0f;
+    [SerializeField] private float jumpHeight = 5.0f;
+    [SerializeField] private float airControl = 0.5f;
 
     private Vector2 input;
     private Rigidbody rigidbody;
+
+    private bool sprinting;
+    private bool jumping;
+
+    private bool grounded = false;
 
     // Start is called before the first frame update
     void Start()
@@ -21,11 +29,50 @@ public class Movement : MonoBehaviour
     {
         input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         input.Normalize();
+
+        sprinting = Input.GetButton("Sprint");
+        jumping = Input.GetButton("Jump");
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        grounded = true;
     }
 
     private void FixedUpdate()
     {
-        rigidbody.AddForce(CalculateMovement(walkSpeed), ForceMode.VelocityChange);
+        if(grounded)
+        {
+            if (jumping)
+            {
+                rigidbody.velocity = new Vector3(rigidbody.velocity.x, jumpHeight, rigidbody.velocity.z);
+            }
+            else if (input.magnitude > 0.5f)
+            {
+                rigidbody.AddForce(CalculateMovement(sprinting ? runSpeed : walkSpeed), ForceMode.VelocityChange);
+            }
+            else
+            {
+                var velocity1 = rigidbody.velocity;
+                velocity1 = new Vector3(velocity1.x * 0.2f * Time.fixedDeltaTime, velocity1.y, velocity1.z * 0.2f * Time.fixedDeltaTime);
+                rigidbody.velocity = velocity1;
+            }
+        } 
+        else
+        {
+            if (input.magnitude > 0.5f)
+            {
+                rigidbody.AddForce(CalculateMovement(sprinting ? runSpeed : walkSpeed), ForceMode.VelocityChange);
+            }
+            else
+            {
+                var velocity1 = rigidbody.velocity;
+                velocity1 = new Vector3(velocity1.x * 0.2f * Time.fixedDeltaTime, velocity1.y, velocity1.z * 0.2f * Time.fixedDeltaTime);
+                rigidbody.velocity = velocity1;
+            }
+        }
+        
+        grounded = false;
     }
 
     Vector3 CalculateMovement(float _speed) 
